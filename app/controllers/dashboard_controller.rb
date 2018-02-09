b3a4cae65b8b16d82ccb75493a70ec8c0382f341
Hardik <hardikpatel1644@gmail.com>
2018-02-08 17:54:17 -0700

    - Added User listing with fake user entries in database

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class DashboardController < ApplicationController
+  before_action :logged_in_user, only: [:index]
   def index
   end
+
+  private
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
